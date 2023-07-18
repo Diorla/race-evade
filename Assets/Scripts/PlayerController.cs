00aa22220a55ebbb054ec81bd5ec3b0a58c0108e
Ade Adeola <adedotster@gmail.com>
2023-07-18 22:05:26 +0100

    add controls to vehicle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 5.0f;
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +16,15 @@ public class PlayerController : MonoBehaviour
 
      // Update is called once per frame
      void Update()
-     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 15);
+    {
+        // Get player inputs: arrows or a-w-d-s keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // up indicates the y-axis
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
      }
 
 }
