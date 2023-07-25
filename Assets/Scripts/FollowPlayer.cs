aea182e623fa212f99a2ff4e9808f5c2b7354e5b
Ade Adeola <adedotster@gmail.com>
2023-07-25 20:09:38 +0100

    camera switching

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -10);
+    public string cameraType = "far";
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,18 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(cameraType == "far")
+            {
+                cameraType = "close";
+                offset = new Vector3(0, 5, -3);
+            } else
+            {
+                cameraType = "far";
+                offset = new Vector3(0, 5, -10);
+            }
+        }
         // Offset the camera behind the player position
         transform.position = player.transform.position + offset;
     }
