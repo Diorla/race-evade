ada897560eddc09154c2b52429497187eff26e73
Ade Adeola <adedotster@gmail.com>
2023-07-29 10:22:09 +0100

    multiplayer added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public string inputID;
     private float speed = 5.0f;
     private float turnSpeed = 25f;
     private float horizontalInput;
     private float verticalInput;
     // Start is called before the first frame update
+    // TODO: Make car mass affects its speed and maneuverability.
     void Start()
     {
 
@@ -18,8 +20,8 @@ public class PlayerController : MonoBehaviour
      void Update()
     {
         // Get player inputs: arrows or a-w-d-s keyboard
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        verticalInput = Input.GetAxis("Vertical" + inputID);
 
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
